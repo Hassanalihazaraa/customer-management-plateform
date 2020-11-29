6c12c3f957789f2657da2f3d8363cd5830b31299
Hassan <hassanalihazaraa@gmail.com>
2020-11-29 13:22:54 +0100

    renamed customers to customer and rgistered the model customer to admin site

--- account/models.py
+++ account/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 
-class Customers(models.Model):
+class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-    
+    def __str__(self) -> str:
+        return self.name
