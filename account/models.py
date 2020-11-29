ec8f84364689d6a355c904e80f1ee45501ccc08b
Hassan <hassanalihazaraa@gmail.com>
2020-11-29 14:04:39 +0100

    added relations to Product instead of orders

--- account/models.py
+++ account/models.py
@@ -12,6 +12,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -22,13 +29,7 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=200, null=True)
-
-    def __str__(self) -> str:
-        return self.name
+    tags = models.ManyToManyField(Tag)
 
 
 class Order(models.Model):
@@ -47,4 +48,3 @@ class Order(models.Model):
         on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
-    tags = models.ManyToManyField(Tag)
