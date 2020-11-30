435f51fe6ba6e44735c98db507ceb03f0646455c
Hassan <hassanalihazaraa@gmail.com>
2020-11-30 14:33:07 +0100

    added success message

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.forms import inlineformset_factory
+from django.contrib import messages
 
 from .models import *
 from .forms import OrderForm, CreateUserForm
@@ -12,6 +13,8 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
             return redirect('login')
 
     context = {
